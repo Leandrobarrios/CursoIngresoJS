4af122d0b203eef8f9ea762ce93257dba727d835
Leandrobarrios <leandrozabh@gmail.com>
2016-11-18 15:47:34 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,58 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+
+	precio1 = document.getElementById('PrecioUno').value;
+	precio2 = document.getElementById('PrecioDos').value;
+	precio3 = document.getElementById('PrecioTres').value;
+
+	precio1 = parseInt (precio1);
+	precio2 = parseInt (precio2);
+	precio3 = parseInt (precio3);
+
+	suma = (precio1 + precio2 + precio3);
+	alert (suma);
+
 }
 function Promedio () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var promedio;
+
+	precio1 = document.getElementById('PrecioUno').value;
+	precio2 = document.getElementById('PrecioDos').value;
+	precio3 = document.getElementById('PrecioTres').value;
+
+	precio1 = parseInt (precio1);
+	precio2 = parseInt (precio2);
+	precio3 = parseInt (precio3);
+
+	promedio = (precio1 + precio2 + precio3)/3;
+	alert (promedio);
 }
 function PrecioFinal () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var precioparcial;
+	var iva;
+
+	precio1 = document.getElementById('PrecioUno').value;
+	precio2 = document.getElementById('PrecioDos').value;
+	precio3 = document.getElementById('PrecioTres').value;
+
+	precio1 = parseInt (precio1);
+	precio2 = parseInt (precio2);
+	precio3 = parseInt (precio3);
+	precioparcial = (precio1 + precio2 + precio3);
+	iva = (precioparcial*21/100);
+	preciofinal=(precioparcial+iva);
+	alert (preciofinal);
 }
