27e079d06a6aaf2c8085d90da15af85602de45c7
Leandrobarrios <leandrozabh@gmail.com>
2016-11-21 09:45:06 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad = document.getElementById('edad').value;
+if (edad=="15"){alert("mensaje");
+
+
+}//fin if (edad==15)
+
+
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,20 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad
+edad = document.getElementById('edad').value;
+/*{
+if(edad<=17)
+{
+alert( "usted es un adolescente");
+}
 
+}*/
+
+if ( edad>=13 && edad<=17)
+{
+	alert  (" es adolescente");
+}
+alert ("por aca pasa siempre");
 
 }//FIN DE LA FUNCIÓN
