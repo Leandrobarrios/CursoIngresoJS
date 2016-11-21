a7a4e854400259f8b91eca822a5472f5df16e038
Leandrobarrios <leandrozabh@gmail.com>
2016-11-21 11:46:50 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,18 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+var edad;
+var estadocivil;
+
+edad=document.getElementById('edad').value;
+estadocivil=document.getElementById('estadoCivil').value;
+ 
+if (edad>18 && estadocivil == "Soltero")
+	{
+		alert ("es soltero y no menor");
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
