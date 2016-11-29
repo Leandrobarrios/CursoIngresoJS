a52bf9d9c49c118d2db6eef2120eae6608c8362f
Leandrobarrios <leandrozabh@gmail.com>
2016-11-29 17:01:20 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,8 +3,37 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)
+{
+	default:
+		alert("ya pasamos el frio, ahora calor!!!");
+		break;
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+		alert ("falta para el invierno");
+		break;
+	
+	case "Julio":
+	case "Agosto":
+		alert ("Abrigate que hace frio.");
+		break;		
+	
 
+
+
+
+
+	
+
+
+
+
+
+}
 
 
 
