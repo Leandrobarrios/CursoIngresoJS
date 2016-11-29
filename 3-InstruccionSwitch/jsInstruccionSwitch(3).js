1cfc91f472a2d4e7e0e7cfc3a9c7ffaf6fe84e0a
Leandrobarrios <leandrozabh@gmail.com>
2016-11-29 17:22:57 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -6,6 +6,17 @@ var mesDelAño = document.getElementById('mes').value;
 //alert (mesDelAño);
 	
 	
+switch (mesDelAño)
+{
+
+case "Febrero":
+	alert ("este mes no tiene mas de 29 dias");
+	break;
+default :
+	alert ("este mes tiene mas de 30 dias o mas");
+	break;
+
+}
 
 
 }//FIN DE LA FUNCIÓN
